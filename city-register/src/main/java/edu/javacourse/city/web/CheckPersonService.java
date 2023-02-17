d5cee414947164fa1831b84972b51e020627ac04
Zaychik <You@example.com>
2023-02-17 16:57:05 +0300

    After rest

---
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -0,0 +1,40 @@
+package edu.javacourse.city.web;
+
+import edu.javacourse.city.dao.PersonCheckDao;
+import edu.javacourse.city.dao.PoolConnectionBuilder;
+import edu.javacourse.city.domain.PersonRequest;
+import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckException;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDate;
+
+
+@Path("/check")
+public class CheckPersonService {
+    private static final Logger logger = LoggerFactory.getLogger(CheckPersonService.class);
+
+    private PersonCheckDao dao;
+
+    @PostConstruct
+    public void init() {
+        logger.info("SERVICE is created");
+        dao = new PersonCheckDao();
+        dao.setConnectionBuilder(new PoolConnectionBuilder());
+    }
+
+    @POST
+    @Singleton
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
+        logger.info(request.toString());
+        return  dao.checkPerson(request);
+    }
+}
+
